905bbb7625768a9129ed0b11a8569cd5c1c0b43b
ttobe <ahdrmfgur12@gmail.com>
2024-06-10 22:53:24 +0900

    chore: spring 프로젝트 설정

---
+++ src/main/java/com/springsqllab/springsqllab/SpringsqllabApplication.java
@@ -0,0 +1,13 @@
+package com.springsqllab.springsqllab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsqllabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsqllabApplication.class, args);
+	}
+
+}
