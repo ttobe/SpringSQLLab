5eabee1fd0ab2cc8b0d72dcde56536ea202645fe
ttobe <ahdrmfgur12@gmail.com>
2024-06-18 15:46:06 +0900

    feat: 파싱 후 SELECT 가능, 프린트 함수 분리

--- src/main/java/com/springsqllab/springsqllab/service/QueryService.java
+++ src/main/java/com/springsqllab/springsqllab/service/QueryService.java
@@ -1,5 +1,7 @@
 package com.springsqllab.springsqllab.service;
 
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.select.Select;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -17,36 +19,54 @@ public class QueryService {
     @Value("${spring.datasource.password}")
     private String password;
 
-    public String executeQuery(String problemId, String query) throws Exception {
+    public String executeQuery(String problemId, String query) {
+        // SQL 검증
+        if (!isSelectQuery(query)) {
+            throw new IllegalArgumentException("Only SELECT queries are allowed.");
+        }
 
-        String dbUrl = url + problemId; // 문제 ID를 URL에 추가하는 방식
+        String dbUrl = url + problemId; // 문제 ID를 URL 에 추가하는 방식
 
         try (Connection conn = DriverManager.getConnection(dbUrl, username, password);
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(query)) {
-
-            // Process ResultSet and format results
-            StringBuilder result = new StringBuilder();
-            ResultSetMetaData metaData = rs.getMetaData();
-            int columnCount = metaData.getColumnCount();
-
-            // Append column names as the header
-            for (int i = 1; i <= columnCount; i++) {
-                result.append(metaData.getColumnName(i)).append("\t");
-            }
-            result.append("\n");
-
-            // Append rows
-            while (rs.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    result.append(rs.getString(i)).append("\t");
-                }
-                result.append("\n");
-            }
-
-            return result.toString();
+            // print result like table
+            return printResult(rs);
         } catch (Exception e) {
-            throw e; // Rethrow the exception
+            return e.getMessage();
         }
     }
+
+    private String printResult(ResultSet rs) throws SQLException {
+        // Process ResultSet and format results
+        StringBuilder result = new StringBuilder();
+        ResultSetMetaData metaData = rs.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
+        // Append column names as the header
+        for (int i = 1; i <= columnCount; i++) {
+            result.append(metaData.getColumnName(i)).append("\t");
+        }
+        result.append("\n");
+
+        // Append rows
+        while (rs.next()) {
+            for (int i = 1; i <= columnCount; i++) {
+                result.append(rs.getString(i)).append("\t");
+            }
+            result.append("\n");
+        }
+
+        return result.toString();
+    }
+
+    private boolean isSelectQuery(String query) {
+        try {
+            Select select = (Select) CCJSqlParserUtil.parse(query);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
